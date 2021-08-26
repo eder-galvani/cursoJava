1d2c683d2fc9e8206a85e288dd5c6ca87cc3d5be
eder.silva <eder.silva@BRNCSP010049.abnote.int>
2021-08-26 01:18:21 -0300

    construtores

--- exercicios/src/classes/Data.java
+++ exercicios/src/classes/Data.java
@@ -5,4 +5,13 @@ public class Data {
 	int dia;
 	int mes;
 	int ano ;
+	
+	String ObterDataFormatada() {
+		String dataFormatada = dia + "/"+ mes + "/" + ano;
+		return dataFormatada;
+		
+		/*resposta do professor
+		return String.format("%d%d%d", dia, mes, ano);
+		*/
+	}
 }
--- exercicios/src/classes/Produto.java
+++ exercicios/src/classes/Produto.java
@@ -4,5 +4,12 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto ;
+	//não precisa de parametros quando a classe ja tem tudo que precisamos para o calculo
+	double precoComDesconto() {
+		return preco * (1 - desconto);
+	}
 	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - (desconto + descontoDoGerente));
+	}
 }
