b40002e210f5aebc6c33e8eb315071bfd3b1a0a0
eder.silva <eder.silva@BRNCSP010049.abnote.int>
2021-09-21 01:47:13 -0300

    Desafio conclusao módulo classes e metodos

---
+++ exercicios/src/classes/desafio/Comida.java
@@ -0,0 +1,19 @@
+package classes.desafio;
+
+public class Comida {
+	String nomeComida;
+	double pesoComida;
+	
+	Comida (String nomeComida, double pesoComida){
+		this.nomeComida = nomeComida;
+		this.pesoComida = pesoComida;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
