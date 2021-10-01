db96aa896eb4f11c5f37a0293cf3d19abf8d7359
Eder Galvani <eder.silva@valid.com>
2021-10-01 18:04:32 -0300

    Matriz

--- exercicios/src/controle/BreakRotulado.java
+++ exercicios/src/controle/BreakRotulado.java
@@ -4,7 +4,7 @@ public class BreakRotulado {
 
 	public static void main(String[] args) {
 		externo: for (int i = 0; i < 3; i++) {
-			interno: for (int j = 0; j < 3; j++) {
+			for (int j = 0; j < 3; j++) {
 // se não usar o rotulo o break sai do laço mais interno, usando o rótulo ele sai do rótulo indicado
 				if (i == 1) {
 					break externo;
--- exercicios/src/controle/ContinueRotulado.java
+++ exercicios/src/controle/ContinueRotulado.java
@@ -3,7 +3,7 @@ package controle;
 public class ContinueRotulado {
 	public static void main(String[] args) {
 		externo: for (int i = 0; i < 3; i++) {
-			interno: for (int j = 0; j < 3; j++) {
+			for (int j = 0; j < 3; j++) {
 // se não usar o rotulo o continue sai do laço mais interno, usando o rótulo ele sai do rótulo indicado
 				if (i == 1) {
 					continue externo;
