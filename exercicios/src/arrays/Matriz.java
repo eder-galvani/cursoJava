c152833e0aea944196dc5791a014721321b22e28
eder.silva <eder.silva@BRNCSP010049.abnote.int>
2022-05-13 02:03:18 -0300

    excecoes

--- exercicios/src/arrays/Matriz.java
+++ exercicios/src/arrays/Matriz.java
@@ -1,5 +1,6 @@
 package arrays;
 
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,30 +10,29 @@ public class Matriz {
 
 		System.out.print("Quantos Alunos? ");
 		int qtdAlunos = entrada.nextInt();
-  
+
 		System.out.print("Quantas notas por aluno? ");
 		int qtdNotas = entrada.nextInt();
-		
-		double [] [] notasDaTurma = new double [qtdAlunos] [qtdNotas];
+
+		double[][] notasDaTurma = new double[qtdAlunos][qtdNotas];
 		double total = 0;
-		
+
 		for (int a = 0; a < notasDaTurma.length; a++) {
 			for (int n = 0; n < notasDaTurma[a].length; n++) {
-				
+
 				System.out.printf("Informe a nota %d do aluno %d: ", n + 1, a + 1);
-				
-				notasDaTurma [a] [n] = entrada.nextDouble();
-				total += notasDaTurma [a][n];
-							
+
+				notasDaTurma[a][n] = entrada.nextDouble();
+				total += notasDaTurma[a][n];
+
 			}
 		}
-		
+
 		double media = total / (qtdAlunos * qtdNotas);
 		System.out.println("Média da turma é " + media);
-		
+
 		System.out.println(Arrays.deepToString(notasDaTurma));
-		
-		
+
 		entrada.close();
 	}
 }
