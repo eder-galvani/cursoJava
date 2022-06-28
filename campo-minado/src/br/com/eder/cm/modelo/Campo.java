a3b6b8189bd73e4cffd8e291b46cccd051d306ad
eder.silva <eder.silva@BRNCSP010049.abnote.int>
2022-06-28 01:57:47 -0300

    Métodos e testes da classe campo

--- campo-minado/src/br/com/eder/cm/modelo/Campo.java
+++ campo-minado/src/br/com/eder/cm/modelo/Campo.java
@@ -48,16 +48,16 @@ public class Campo {
 
 		}
 	}
-	
+
 	boolean abrir() {
-		
+
 		if (!aberto && !marcado) {
 			aberto = true;
-			
+
 			if (minado) {
 				throw new ExplosaoException();
-				}
-			
+			}
+
 			if (vizinhancaSegura()) {
 				vizinhos.forEach(v -> v.abrir());
 			}
@@ -65,12 +65,74 @@ public class Campo {
 		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
+
+	void minar() {
+		minado = true;
+	}
+
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	public boolean isAberto() {
+		return aberto;
+	}
+	
+	public boolean isFechado() {
+		return !isAberto();
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+	
+	long minasNaVizinhanca() {
+		return vizinhos.stream().filter(v -> v.minado).count();
+	}
+	
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	}
+	
+	public String toString() {
+		if (marcado) {
+			return "x";
+		} else if(aberto && minado) {
+			return "*";
+		} else if(aberto && minasNaVizinhanca() > 0) {
+			return Long.toString(minasNaVizinhanca());
+		} else if(aberto) {
+			return " ";
+		} else {
+			return "?";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
